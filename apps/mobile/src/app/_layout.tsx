d9f0101683429b53600302bffa396d7faa9ec551
Mario Chacon <53308354+masch@users.noreply.github.com>
2026-04-03 15:25:49 -0300

    chore(mobile): add nativewind for Tailwind CSS support (#19)
    * chore(mobile): add nativewind for Tailwind CSS support
    
    * chore(mobile): fix dependency versions to match Expo SDK 54

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 import { DEFAULT_SCREEN_OPTIONS } from "../constants/nav.constants";
+import "../../global.css"
 
 export default function RootLayout() {
   return <Stack screenOptions={DEFAULT_SCREEN_OPTIONS} />;
