dffe860d9413afb88a28a3102cd62435a3b5e895
masch <the.masch@gmail.com>
2026-05-02 16:38:20 -0300

    fix: cast salt to BufferSource in PBKDF2 derivation to satisfy type requirements

--- apps/backend/src/services/password.service.ts
+++ apps/backend/src/services/password.service.ts
@@ -29,7 +29,7 @@ async function deriveKey(
   return crypto.subtle.deriveBits(
     {
       name: PBKDF2_ALGORITHM,
-      salt: salt as Uint8Array,
+      salt: salt as unknown as BufferSource,
       iterations,
       hash: PBKDF2_HASH_ALGO,
     },
